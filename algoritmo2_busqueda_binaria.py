66b0d9b2e574dd0ec194f95454d979f484f864c4
Jhoan M. Chávez <149123969+Jhoanmco@users.noreply.github.com>
2025-10-03 19:41:41 -0500

    algoritmo2_busqueda_binaria.py

--- algoritmo2_busqueda_binaria.py
+++ algoritmo2_busqueda_binaria.py
@@ -2,10 +2,10 @@ def busqueda_binaria(arr, objetivo):
     #caso base: la lista vacía
     if not arr:
         return -1
-    
+
     n = len(arr)
     mitad = n // 2
-    
+
     if arr[mitad] == objetivo:
         return mitad
     elif arr[mitad] > objetivo:
@@ -16,13 +16,13 @@ def busqueda_binaria(arr, objetivo):
 
 #ejemplo
 lista_ordenada = [1, 5, 8, 12, 16, 23, 38, 56, 72, 91]
-elemento_buscado = 23
+elemento_buscado = 12
 
 indice = busqueda_binaria(lista_ordenada, elemento_buscado)
 
 print(f"Lista: {lista_ordenada}")
 if indice != -1:
-    print(f"El elemento {elemento_buscado} se encontró en el índice: {indice}")
+    print(f"El elemento {elemento_buscado} se encontró en el índice: {indice} (indices: 0,1,2, ..., n)")
 else:
     print(f"El elemento {elemento_buscado} no se encontró en la lista.")
 
