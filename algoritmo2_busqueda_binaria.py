e3e3f57425d667fe9df27d210ea567fabf4f8629
Jhoan M. Chávez <149123969+Jhoanmco@users.noreply.github.com>
2025-10-03 19:25:52 -0500

    algoritmo2_busqueda_binaria.py

--- algoritmo2_busqueda_binaria.py
+++ algoritmo2_busqueda_binaria.py
@@ -1,33 +1,20 @@
 def busqueda_binaria(arr, objetivo):
-    """
-    Implementa la búsqueda binaria de forma recursiva.
-    Asume que la lista 'arr' está ORDENADA.
-    """
-    # Caso base: la lista está vacía
+    #caso base: la lista vacía
     if not arr:
         return -1
     
     n = len(arr)
     mitad = n // 2
     
-    # Caso base: el elemento se encuentra en la mitad
     if arr[mitad] == objetivo:
         return mitad
-    
-    # Caso recursivo: el elemento está en la mitad izquierda
     elif arr[mitad] > objetivo:
-        # Se busca en la primera mitad
         return busqueda_binaria(arr[:mitad], objetivo)
-        
-    # Caso recursivo: el elemento está en la mitad derecha
     else:
-        # Se busca en la segunda mitad y se ajusta el índice
         resultado = busqueda_binaria(arr[mitad+1:], objetivo)
-        
-        # Si se encuentra, se suma el desplazamiento para obtener el índice real
         return resultado if resultado == -1 else mitad + 1 + resultado
 
-# Ejemplo de uso
+#ejemplo
 lista_ordenada = [1, 5, 8, 12, 16, 23, 38, 56, 72, 91]
 elemento_buscado = 23
 
